b9f74adb1947d0b683e40ae0e093af1c17042ec7
HideOnW <william.blanc92@gmail.com>
2023-08-27 12:56:51 +0200

    Barre de Recherche HTML/CSS/JS

--- script/pages/index.js
+++ script/pages/index.js
@@ -1,3 +1,29 @@
+// async function getRecipes() {
+//     return fetch("../../recipes.js") .then((reponse) => reponse.json())
+// }
+
+
+
+// Barre de recherche
+
+const search = document.getElementById("search")
+function searchFiltre(){
+    const inputV = search.value.toLowerCase()
+    const result = recipes.filter((recipe) => recipe.name.toLowerCase().includes(inputV) 
+    || recipe.description.toLowerCase().includes(inputV)
+    // || recipe.ingredients.ingredient.toLowerCase().includes(inputV)
+    || recipe.description.toLowerCase().includes(inputV)
+    || recipe.appliance.toLowerCase().includes(inputV)
+    // || recipe.ustensils.toLowerCase().includes(inputV)
+    )
+    console.log(result)
+    if (result.length >= 1){
+    document.getElementById("recettes").textContent = ""
+    displayData(result)
+    }
+}
+
+// Affichage recettes
 
 async function displayData(recipes) {
     const recettesSection = document.getElementById("recettes")
@@ -11,7 +37,7 @@ async function displayData(recipes) {
 
 async function init(){
     // Récupération des recettes
-    console.log(recipes)
+    // const { recipes } = await getRecipes()
     displayData(recipes)
 }
 
